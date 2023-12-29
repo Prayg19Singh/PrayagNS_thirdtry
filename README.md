523b92e2e20e673ef6d237673781874ddb52e5ba
Prayag <prayagsingh19bbau@gmail.com>
2023-12-29 13:29:23 +0530

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# PrayagNS_thirdtry
---
+++ src/main/java/com/tut/App.java
@@ -0,0 +1,15 @@
+package com.tut;
+
+// import org.hibernate.SessionFactory;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( " Project started....! " );
+
+    }
+}
