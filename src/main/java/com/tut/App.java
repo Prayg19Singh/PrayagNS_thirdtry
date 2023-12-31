58fff7c2d81e8e21ff28b9fa3fdb4012b7e3e442
Prayag <prayagsingh19bbau@gmail.com>
2023-12-31 13:54:44 +0530

    file modified by prayag to understand the revert command

--- src/main/java/com/tut/App.java
+++ src/main/java/com/tut/App.java
@@ -9,7 +9,11 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( " Project started....! " );
-
-    }
+        System.out.println ( " Project started....! " );
+	
+	System.out.println (" I am doing changes in this file to understand the concept of 'git revert' command");
+	    
+	System.out.println (" Commit ID will be also needed to to understand the 'revert' command");
+	    
+	}
 }
