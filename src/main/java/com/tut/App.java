9c109bc9dfc6f5ee829cf76feaf7130dcf21eea8
Prayag <prayagsingh19bbau@gmail.com>
2023-12-31 14:12:54 +0530

    Revert "changes done by Prayag! to understand the revert command"
    This reverts commit a2ed299d6a02ae21e49452204bdd94941377f130.

--- src/main/java/com/tut/App.java
+++ src/main/java/com/tut/App.java
@@ -10,13 +10,10 @@ public class App
     public static void main( String[] args )
     {
         System.out.println ( " Project started....! " );
-	    
-	System.out.println( "************** revert command ********");
-	    
+	
 	System.out.println (" I am doing changes in this file to understand the concept of 'git revert' command");
 	    
 	System.out.println (" Commit ID will be also needed to to understand the 'revert' command");
-	
 	    
 	}
 }
